9e419bd001f34573741b1cd1105bae045fe1ea15
atriple <andiaqil.dpe@gmail.com>
2019-12-26 23:34:36 +0700

    Implement code, add [App, Pet] components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Pet } from './Pet';
+
 const App = () => {
-    return React.createElement("div", {})
+    return React.createElement("div", { id: "main" }, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havaneese"
+        })
+    ]);
 }
 
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
+};
