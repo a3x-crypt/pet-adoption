48cd69a3a8dcafa6beb529e57118f6f3e9577aa7
atriple <andiaqil.dpe@gmail.com>
2019-12-28 01:20:12 +0700

    Change [App, Pet] to JSX syntax

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement("div", { id: "main" }, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "Havaneese"
-        })
-    ]);
-}
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
+};
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
-};
+  return (
+    <div>
+      <h1>{animal}</h1>
+      <h2>{name}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+}
