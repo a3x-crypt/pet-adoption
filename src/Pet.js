01031b5cb25d582d3893e88470412caf87aca2f3
atriple <andiaqil.dpe@gmail.com>
2019-12-27 23:54:12 +0700

    Change [Pet] to export default, add style.css

--- src/Pet.js
+++ src/Pet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Pet = ({ name, animal, breed }) => {
+export default function Pet({ name, animal, breed }) {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, animal),
