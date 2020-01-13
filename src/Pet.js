aa5997c1b7ae2dda08f7b054511514b09de6e414
atriple <andiaqil.dpe@gmail.com>
2020-01-13 10:05:59 +0700

    add [Results] component, update [Pet]

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let image = "http://placecorgi.com/300/300";
+  if (media.length) {
+    image = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{animal}</h1>
-      <h2>{name}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -51,6 +52,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
